ec42932f50aa17628f8c98a3752b9991dc02ed4e
Kanesa-Seia <cisi22033@g.nihon-u.ac.jp>
2024-12-10 11:45:24 +0900

    Change Lox can moving

--- codes/com/craftinginterpreters/lox/Lox.java
+++ codes/com/craftinginterpreters/lox/Lox.java
@@ -40,14 +40,14 @@ public class Lox {
     }
 
     private static void run(String source){
-        /* 
+         
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
 
         //For now, just print the tokens
         for(Token token : tokens){
             System.out.println(token);
-        }*/
+        }
         System.out.println(source);
         
 
