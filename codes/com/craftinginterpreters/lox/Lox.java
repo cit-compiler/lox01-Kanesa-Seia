7f9153fea26655bc0e69cdf696e7903b71a7b7f4
Kanesa-Seia <cisi22033@g.nihon-u.ac.jp>
2024-12-24 11:22:38 +0900

    add RuntimeError.java Interpreter.java

--- codes/com/craftinginterpreters/lox/Lox.java
+++ codes/com/craftinginterpreters/lox/Lox.java
@@ -19,9 +19,12 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+//import java.util.Scanner;
 
 public class Lox {
+    private static final Interpreter interpreter = new Interpreter();
     static boolean hadError = false;
+    static boolean hadRuntimeError = false;
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
           System.out.println("Usage: jlox [script]");
@@ -36,6 +39,7 @@ public class Lox {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         run(new String(bytes, Charset.defaultCharset()));
         if(hadError) System.exit(65);
+        if (hadRuntimeError) System.exit(70);
       }
 
     private static void runPrompt() throws IOException{
@@ -62,7 +66,8 @@ public class Lox {
         // Stop if there was a syntax error.
         if (hadError) return;
     
-        System.out.println(new AstPrinter().print(expression));
+        //System.out.println(new AstPrinter().print(expression));
+        interpreter.interpret(expression);
 
     }
 
@@ -70,20 +75,25 @@ public class Lox {
         report(line, "", message);
     }
 
-    private static void report(int line, String where, String message){
-        
-        System.err.println(
-            "[line" + line + "] Error" + where + ": " + message);
-            hadError = true;
-        }
-    
-      static void error(Token token, String message) {
-    if (token.type == TokenType.EOF) {
-      report(token.line, " at end", message);
-    } else {
-      report(token.line, " at '" + token.lexeme + "'", message);
+    private static void report(int line, String where, String message){ 
+      System.err.println(
+          "[line" + line + "] Error" + where + ": " + message);
+          hadError = true;
     }
-  }
+    
+    static void error(Token token, String message) {
+      if (token.type == TokenType.EOF) {
+        report(token.line, " at end", message);
+      } else {
+        report(token.line, " at '" + token.lexeme + "'", message);
+      }
+    }
+
+    static void runtimeError(RuntimeError error) {
+      System.err.println(error.getMessage() +
+          "\n[line " + error.token.line + "]");
+      hadRuntimeError = true;
+    }
 
 }
 
